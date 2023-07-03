7d2c96cd1a64f75b9e20aa56e0c33948406e2d02
t.urbutt <tim@urbutt.de>
2023-07-03 12:26:33 +0200

    add version number

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:wayaware_app/backend/authentication.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,17 +11,26 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _scaleAnimation;
 
+  String versionNumber = "";
+
   final double _initialScale = 1.0;
-  final double _targetScale = 1.1;
-  final int _animationDuration = 1000; // Adjust the animation duration (in milliseconds)
+  final double _targetScale = 1.05;
+  final int _animationDuration =
+      3000; // Adjust the animation duration (in milliseconds)
 
   @override
   void initState() {
-    super.initState();
+    PackageInfo.fromPlatform().then((packageInfo) {
+      setState(() {
+        versionNumber = packageInfo.version;
+        print("test");
+      });
+    });
     _animationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: _animationDuration),
@@ -35,6 +45,7 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
         curve: Curves.easeInOut,
       ),
     );
+    super.initState();
   }
 
   @override
@@ -46,82 +57,106 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 100,),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: AnimatedBuilder(
-                  animation: _scaleAnimation,
-                  builder: (context, child) {
-                    return Transform.scale(
-                      scale: _scaleAnimation.value,
-                      child: Image.asset(
-                        'assets/app_icon.png',
-                        width: 350.0,
+      backgroundColor: Colors.grey.shade300,
+      body: Stack(
+        children: [
+          Positioned.fill(
+              child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.white, Colors.grey.shade300],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          )),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: AnimatedBuilder(
+                        animation: _scaleAnimation,
+                        builder: (context, child) {
+                          return Transform.scale(
+                            scale: _scaleAnimation.value,
+                            child: Image.asset(
+                              'assets/app_icon_transparent.png',
+                              width: 200.0,
+                            ),
+                          );
+                        }),
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 65,
+                    child: ElevatedButton.icon(
+                      onPressed: () => Authentication.signInWithApple(),
+                      icon: Padding(
+                        padding: const EdgeInsets.only(right: 3),
+                        child: SizedBox(
+                          height: 20,
+                          child: Image.network(
+                              'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Apple_logo_white.svg/1724px-Apple_logo_white.svg.png',
+                              fit: BoxFit.cover),
+                        ),
                       ),
-                    );
-                  }),
-            ),
-            const Spacer(),
-            SizedBox(
-              width: 350,
-              height: 65,
-              child: ElevatedButton.icon(
-                onPressed: () => Authentication.signInWithGoogle(),
-                icon: Padding(
-                  padding: const EdgeInsets.only(left: 1.0),
-                  child: SizedBox(
+                      label: const Text('Login with Apple'),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 65,
+                    child: ElevatedButton.icon(
+                      onPressed: () => Authentication.signInWithGoogle(),
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 1.0),
+                        child: SizedBox(
+                          height: 20,
+                          child: Image.network(
+                              'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png',
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                      label: const Text('Login with Google'),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
                     height: 20,
-                    child: Image.network(
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png',
-                        fit: BoxFit.cover),
                   ),
-                ),
-                label: const Text('Login with Google'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                  Text(
+                    "Version $versionNumber",
+                    style: TextStyle(fontSize: 14, color: Colors.grey.shade800),
                   ),
-                ),
+                  const SizedBox(
+                    height: 50,
+                  )
+                ],
               ),
             ),
-            const SizedBox(height: 16.0),
-            SizedBox(
-              width: 350,
-              height: 65,
-              child: ElevatedButton.icon(
-                onPressed: () => Authentication.signInWithApple(),
-                icon: Padding(
-                  padding: const EdgeInsets.only(right: 3),
-                  child: SizedBox(
-                    height: 20,
-                    child: Image.network(
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Apple_logo_white.svg/1724px-Apple_logo_white.svg.png',
-                        fit: BoxFit.cover),
-                  ),
-                ),
-                label: const Text('Login with Apple'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 100,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
