8ca6f4ea15692f8a3b3b695441dee9c9a59216fc
Finn <dereinefinn@gmail.com>
2023-07-03 12:36:04 +0200

    fix some errors

--- lib/backend/authentication.dart
+++ lib/backend/authentication.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
@@ -23,28 +24,30 @@ class Authentication {
   }
 
   static Future<void> signInWithApple() async {
-    final appleIdCredential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-      webAuthenticationOptions: WebAuthenticationOptions(
-        clientId: 'dev.nordify.wayawarelogin',
-        redirectUri: Uri.parse(
-          'https://wayaware-app.firebaseapp.com/__/auth/handler',
+    try {
+      final appleIdCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        webAuthenticationOptions: WebAuthenticationOptions(
+          clientId: 'dev.nordify.wayaware',
+          redirectUri: Uri.parse(
+            'https://wayaware-app.firebaseapp.com/__/auth/handler',
+          ),
         ),
-      ),
-    );
+      );
 
-    if (appleIdCredential.identityToken == null) return;
+      if (appleIdCredential.identityToken == null) return;
 
-    // get an OAuthCredential
-    final credential = OAuthProvider('apple.com').credential(
-      idToken: appleIdCredential.identityToken,
-      accessToken: appleIdCredential.authorizationCode,
-    );
+      // get an OAuthCredential
+      final credential = OAuthProvider('apple.com').credential(
+        idToken: appleIdCredential.identityToken,
+        accessToken: appleIdCredential.authorizationCode,
+      );
 
-    // use the credential to sign in to firebase
-    await FirebaseAuth.instance.signInWithCredential(credential);
+      // use the credential to sign in to firebase
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } on SignInWithAppleAuthorizationException catch (_) {}
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,6 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp();
+  await FirebaseAuth.instance.signOut();
 
   runApp(MultiBlocProvider(
     providers: [
